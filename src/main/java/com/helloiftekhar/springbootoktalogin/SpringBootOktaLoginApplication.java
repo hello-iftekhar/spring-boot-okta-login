4b8c46bde0a8367f58abb6faa832e4fde97a6e0e
Iftekhar Hossain <helloiftekhar18@gmail.com>
2024-01-25 16:59:08 +0600

    initial commit

---
+++ src/main/java/com/helloiftekhar/springbootoktalogin/SpringBootOktaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.helloiftekhar.springbootoktalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOktaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOktaLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloiftekhar/springbootoktalogin/SpringBootOktaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloiftekhar.springbootoktalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOktaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
